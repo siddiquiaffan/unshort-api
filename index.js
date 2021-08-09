c58542ebf44be6112dff643c483b9d14fd128a51
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-08-09 10:49:35 +0530

    fix unshortening of some urls

--- index.js
+++ index.js
@@ -4,41 +4,33 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/",(req,res)=>{
-  const url = req.query.url;
-  if(url){
-    if (validUrl.isUri(url)) {
-      axios.get(url)
-        .then((response) => {
-          const longUrl = response.request.res.responseUrl;
-          const error = {ok : false , err : 'The link is not shortened!!' , by : 'https://github.com/AffanTheBest'}
-          const result = {ok : longUrl ? 'true' : 'false' ,short_url : url , extracted_url : longUrl ? longUrl : 'Link is not valid', by : 'https://github.com/AffanTheBest'};
-          if(longUrl == url){
-            res.end(JSON.stringify(error));
-          }else{
-            res.end(JSON.stringify(result));
-          }
-        })
-        .catch((err) => {
-          if(err){
-            const longUrl = err.request._options.href || err.request._currentUrl;
-            const error = {ok : false , err : 'The link is not shortened!!' , by : 'https://github.com/AffanTheBest'}
-            const result = {ok : longUrl ? 'true' : 'false' ,short_url : url , extracted_url : longUrl ? longUrl : 'Link is not valid', by : 'https://github.com/AffanTheBest'};
-            if(longUrl == url){
-              res.end(JSON.stringify(error));
-            }else{
-              res.end(JSON.stringify(result));
-            }
-          }
-        })
-    } else {
-      const error = {ok : false , err : 'Either the URL you entered is invalid or not a URL.'}
-      res.end(JSON.stringify(error));
+const unshort = async(url) => {
+  const extractUrl = req => req.request.res.responseUrl ? req.request.res.responseUrl : 
+  req.request._redirectable._currentUrl ? req.request._redirectable._currentUrl : 
+  req.request._currentUrl ? req.request._currentUrl : 
+  req.request._options.href ? req.request._options.href : 
+  'https://'+req.request.host + req.request.path;
+
+    try{
+        const req = await axios.get(url);
+        const result = extractUrl(req);
+        var longUrl = result ? result : url;
+    }catch(err){
+        const result = extractUrl(err);
+        var longUrl = result ? result : url;
     }
+  return {ok: true, shortUrl: url, longUrl, by : 'https://github.com/AffanTheBest'}
+}
+
+app.get("/api/",( async(req,res)=>{
+  const url = req.query.url;
+  if(url && validUrl.isUri(url)){
+    const result = await unshort(url);
+    res.send(JSON.stringify(result));
   }else{
     const error = {ok : false , err : 'URL not found!!' , soln : 'Head to https://github.com/AffanTheBest to know how to use.'};
     res.send(JSON.stringify(error));
   }
-});
+}));
 
 app.listen(port , () => console.log('Listening to the port ' + port));
